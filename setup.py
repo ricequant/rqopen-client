1e7d75093b0f4e3e218cc4270ea21dfecaff5ca9
shidenggui <903618848@qq.com>
2018-05-23 08:44:25 +0800

    :star: support pip10

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ from setuptools import (
     setup,
 )
 
-from pip.req import parse_requirements
-
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 setup(
     name='rqopen-client',
@@ -19,6 +21,9 @@ setup(
     author_email='public@ricequant.com',
     url="https://github.com/ricequant/rqopen-client",
     package_data={'': ['*.*']},
-    install_requires=[str(ir.req) for ir in parse_requirements("requirements.txt", session=False)],
+    install_requires=[
+        str(ir.req)
+        for ir in parse_requirements("requirements.txt", session=False)
+    ],
     zip_safe=False,
 )
